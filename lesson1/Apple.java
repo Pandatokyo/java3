e208b811257ce8c36ae60ec7b5f1dc163720341c
Pandatokyo <99756016+Pandatokyo@users.noreply.github.com>
2022-07-21 19:47:31 +0300

    Delete lesson1 directory

--- lesson1/Apple.java
+++
@@ -1,7 +0,0 @@
-package java3.lesson1;
-
-public class Apple extends Fruit{
-    public Apple() {
-        super(1.0f);
-    }
-}
--- lesson1/Orange.java
+++
@@ -1,7 +0,0 @@
-package java3.lesson1;
-
-public class Orange extends Fruit{
-    public Orange() {
-        super(1.5f);
-    }
-}
