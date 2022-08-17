c949753203d85d2a447903c468df301d53f78bb0
Pandatokyo <dorianmine@mail.ru>
2022-08-17 09:30:32 +0300

    Homework5

---
+++ lesson5/Tunnel.java
@@ -0,0 +1,33 @@
+package src.lesson5;
+
+import java.util.concurrent.Semaphore;
+
+public class Tunnel extends Stage {
+    public Tunnel() {
+        this.length = 80;
+        this.description = "Тоннель " + length + " метров";
+    }
+    static Semaphore semaphore;
+    static {
+        semaphore = new Semaphore(Main.CARS_COUNT / 2);
+    }
+    @Override
+    public void go(Car c) {
+        try {
+            try {
+                System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
+                semaphore.acquire();
+                System.out.println(c.getName() + " начал этап: " + description);
+                Thread.sleep(length / c.getSpeed() * 1000L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                semaphore.release();
+                System.out.println(c.getName() + " закончил этап: " + description);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
